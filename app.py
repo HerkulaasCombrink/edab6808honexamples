669f38265de773e8b31138d1fafad34bf2748baa
HMVE Combrink <44698092+HerkulaasCombrink@users.noreply.github.com>
2025-03-12 14:41:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,4 @@ import numpy as np
 import streamlit as st
 
 st.button("Click me")
-# Generate random time series data
-#time_series = np.random.randn(100)
 
-# Plot the time series
-#plt.plot(time_series)
-#plt.title("Random 100-Unit Time Series")
-#plt.xlabel("Time")
-#plt.ylabel("Value")
-#plt.show()
